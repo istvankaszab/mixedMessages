660b8ea8ba26c4b4147202badb5adf2030ccd870
istvankaszab <istvan.kaszab@gmail.com>
2021-06-17 18:16:45 +0100

    "Refactor word picking"

--- mixedMessages.js
+++ mixedMessages.js
@@ -1,11 +1,14 @@
 const words = require('./words.json');
 
 let message = "";
-const adjective1 = words.adjectives[Math.floor(Math.random() * words.adjectives.length)];
-const noun1 = words.nouns[Math.floor(Math.random() * words.nouns.length)];
-const verb = words.verbs[Math.floor(Math.random() * words.verbs.length)];
-const adjective2 = words.adjectives[Math.floor(Math.random() * words.adjectives.length)];
-const noun2 = words.nouns[Math.floor(Math.random() * words.nouns.length)];
+
+const getRandomWord = (wordClass) => words[wordClass][Math.floor(Math.random() * words[wordClass].length)];
+
+const adjective1 = getRandomWord('adjectives');
+const noun1 = getRandomWord('nouns');
+const verb = getRandomWord('verbs');
+const adjective2 = getRandomWord('adjectives');
+const noun2 = getRandomWord('nouns');
 
 message = `The ${adjective1} ${noun1} ${verb} the ${adjective2} ${noun2}.`;
 console.log(message);
