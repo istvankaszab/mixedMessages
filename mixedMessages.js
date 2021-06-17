a7a32a6f147dc175fec7f5f3bf5b60a32afbaeb2
istvankaszab <istvan.kaszab@gmail.com>
2021-06-17 17:51:27 +0100

    "Pick random words and put the message together"

--- mixedMessages.js
+++ mixedMessages.js
@@ -1,4 +1,11 @@
 const words = require('./words.json');
 
 let message = "";
-console.log(words.adjectives);
+const adjective1 = words.adjectives[Math.floor(Math.random() * words.adjectives.length)];
+const noun1 = words.nouns[Math.floor(Math.random() * words.nouns.length)];
+const verb = words.verbs[Math.floor(Math.random() * words.verbs.length)];
+const adjective2 = words.adjectives[Math.floor(Math.random() * words.adjectives.length)];
+const noun2 = words.nouns[Math.floor(Math.random() * words.nouns.length)];
+
+message = `The ${adjective1} ${noun1} ${verb} the ${adjective2} ${noun2}.`;
+console.log(message);
